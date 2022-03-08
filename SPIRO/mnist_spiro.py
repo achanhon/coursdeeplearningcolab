175b0074370aa48fb68653820f9bff3762a12d43
achanhon <adrien.chan_hon_tong@onera.fr>
2022-03-08 14:52:32 +0100

    +++

---
+++ SPIRO/mnist_spiro.py
@@ -0,0 +1,87 @@
+import torch
+
+
+def compute_accuracy(batchprovider, net):
+    with torch.no_grad():
+        net.eval()
+        net = net.cuda()
+        nb, nbOK = 0, 0
+        for x, y in batchprovider:
+            x, y = x.cuda(), y.cuda()
+            z = net(x)
+
+            _, z = z.max(1)
+            good = (y == z).float()
+            nb += good.shape[0]
+            nbOK += good.sum().cpu().numpy()
+    return nbOK, nb
+
+
+def training_epoch(batchprovider, net, lr, optimizer, criterion):
+    net.train()
+    meanloss = 0
+    nb, nbOK = 0, 0
+    for i, (x, y) in enumerate(batchprovider):
+        x, y = x.cuda(), y.cuda()
+        z = net(x)
+
+        loss = criterion(z, y)
+
+        with torch.no_grad():
+            meanloss += loss.clone().cpu().numpy()
+            _, z = z.max(1)
+            good = (y == z).float()
+            nb += good.shape[0]
+            nbOK += good.sum().cpu().numpy()
+            if i % 50 == 49:
+                print("loss=", meanloss / 50)
+                meanloss = 0
+
+        loss *= lr
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), 10)
+        optimizer.step()
+
+    return nbOK, nb
+
+
+import torchvision
+
+print("load model")
+net = torchvision.models.vgg13(pretrained=True)
+net.avgpool = torch.nn.Identity()
+net.classifier = torch.nn.Linear(512, 10)
+net = net.cuda()
+
+print("load data")
+trainset = torchvision.datasets.CIFAR10(
+    root="build",
+    train=True,
+    download=True,
+    transform=torchvision.transforms.ToTensor(),
+)
+testset = torchvision.datasets.CIFAR10(
+    root="build",
+    train=False,
+    download=True,
+    transform=torchvision.transforms.ToTensor(),
+)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2
+)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=128, shuffle=True, num_workers=2
+)
+
+print("finetune the model on the data")
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.1)
+for epoch in range(8):
+    print("epoch", epoch)
+    nbOK, nb = training_epoch(trainloader, net, 0.0001, optimizer, criterion)
+    print("train accuracy", nbOK / nb)
+
+print("eval model")
+nbok, nb = compute_accuracy(testloader, net)
+print("test accuracy", nbok / nb)
